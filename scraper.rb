c378dcfd9ade3709097cbbc7e12f39e363480fc3
Adrian Short <adrian@adrianshort.org>
2018-10-16 13:28:32 +0100

    Use chained scrape parameters

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 require 'uk_planning_scraper'
 require 'scraperwiki'
 
-apps = UKPlanningScraper::Authority.named(ENV['MORPH_AUTHORITY_NAME']).scrape({ validated_days: ENV['MORPH_DAYS'].to_i })
+apps = UKPlanningScraper::Authority.named(ENV['MORPH_AUTHORITY_NAME']). \
+  validated_days(ENV['MORPH_DAYS'].to_i).scrape
 ScraperWiki.save_sqlite([:authority_name, :council_reference], apps)
